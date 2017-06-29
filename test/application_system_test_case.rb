eb993e39aee9a81434a4395261ee49bd94d75f8e
Celso Fernandes <celso.fernandes@gmail.com>
2017-06-29 09:03:53 -0300

    build!

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV['CHROME_HEADLESS']
+    driven_by :selenium, using: :chrome, options: { args: ['headless'] }
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
+
+  if ENV['CHROME_DRIVER_PATH']
+    Selenium::WebDriver::Chrome.driver_path=ENV['CHROME_DRIVER_PATH']
+  end
 end
