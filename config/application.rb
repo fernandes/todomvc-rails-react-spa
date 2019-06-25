d959d6952d19d1829d7ca9b853ff5bc2cfaf11cc
Celso Fernandes <celso.fernandes@gmail.com>
2019-06-25 15:14:28 +0200

    bump deps

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module TodoApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.trailblazer.enable_loader = false
   end
 end
